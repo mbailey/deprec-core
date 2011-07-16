abc1cf638a3890e31f637e965bc102faf8e2ba76
Mike Bailey <mike@bailey.net.au>
2011-07-17 03:07:56 +1000

    deprec-core-3.1.4

--- lib/deprec-core.rb
+++ lib/deprec-core.rb
@@ -1,11 +1,10 @@
 require "deprec-core/version"
 
 if defined?(Capistrano)
-  require "#{File.dirname(__FILE__)}/deprec-core/capistrano_extensions"
-  require "#{File.dirname(__FILE__)}/vmbuilder_plugins/all"
-  require "#{File.dirname(__FILE__)}/deprec-core/canonical"
-  require "#{File.dirname(__FILE__)}/deprec-core/deprec"
-  require "#{File.dirname(__FILE__)}/deprec-core/deprecated"
+  require "deprec-core/capistrano_extensions"
+  require "vmbuilder_plugins/all"
+  require "deprec-core/canonical"
+  require "deprec-core/deprec"
 elsif defined?(rake)
   # pass
 end
--- lib/deprec-core/version.rb
+++ lib/deprec-core/version.rb
@@ -1,5 +1,5 @@
 module Deprec
   module Core
-    VERSION = "3.1.3"
+    VERSION = "3.1.4"
   end
 end
